ca29cabd56b377fea5aef1084a810f69e7d23b47
PunkIslamist <achondrias@gmail.com>
2017-08-05 17:21:31 +0200

    Created solution and projects

---
+++ src/DriveExplorer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DriveExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
